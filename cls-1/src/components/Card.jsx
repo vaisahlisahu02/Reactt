574d4420197c8800937fbd354c32ae662a1114dc
vaishalisahu02 <sahuvaishali521@gmail.com>
2025-06-23 17:59:50 +0530

    Add cls-1 and cls-2 React projects

---
+++ cls-1/src/components/Card.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+// const Card = (props) => {
+//     console.log(props.username)
+//   return (
+//     <div className='bg-white mr-8 text-black px-9 py-9 inline-block text-center rounded'>
+//         <img className='h-32 mb-7 ml-9 w-32 rounded-full ' src={props.photo} alt="" />
+//      <h2 className='text-2xl mb-4 font-semibold '>{props.username}</h2>   
+//      <h3 className='mb-4 text-2xl'>{props.city}, {props.age}</h3>
+//      <h4 className='text-blue-600'>{props.profession}</h4>
+//      <button className='bg-emerald-900 mt-3 text-white px-4 py-2 rounded'>Add friend</button>
+     
+//     </div>
+//   )
+// }
+
+// export default Card
